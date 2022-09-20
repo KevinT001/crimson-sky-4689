d599b3d47d3735cb6920fa0c809285992f1dd72c
Kevin Ta <kvnta@yahoo.com>
2022-09-20 13:55:17 -0600

    Forgot to save, but getting a Controller show page error, but I am sure everythign is currently there, taking way to long

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,15 +5,26 @@ RSpec.describe 'dishes show page US_1' do
   before :each do 
     @chef1 = Chef.create!(name: "Jordan Hamsey")
     @chef2 = Chef.create!(name: "Bolfgang Shuck")
-    @dish1 = Dish.create!(name: "rotton biscuits", description: "Wormy bread" chef_id: @chef1.id )
+
+    @dish1 = Dish.create!(name: "rotton biscuits", description: "Wormy bread", chef_id: @chef1.id )
     @dish2 = Dish.create!(name: "burnt fish", description: "Charred goldfish", chef_id: @chef1.id )
     @dish3 = Dish.create!(name: "moldy steak", description: "Rare green ribeye", chef_id: @chef2.id )
   
+    @ingredient1 = Ingredient.create!(name: "Fuzzy cheddar", calories: 200 )
+    @ingredient2 = Ingredient.create!(name: "3 eyed fish", calories: 200 )
+    @ingredient3 = Ingredient.create!(name: "mystery meat", calories: 200 )
+    @ingredient4 = Ingredient.create!(name: "Fuzzy cheddar", calories: 200 )
+
     
-  
   end
   it 'I see the dishes name and description' do 
+    visit dish_path(@dish1)
 
+    expect(page).to have_content("#{@dish1.name}")
+    expect(page).to have_content("#{@dish1.dscription}")
+    
+    expect(page).to_not have_content("#{@dish2.name}")
+    expect(page).to_not have_content("#{@dish2.description}")
 
   end
 
